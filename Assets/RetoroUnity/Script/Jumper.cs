2f2c160e560444d9ab5dd9e00960daa202f4e247
jws2170 <mpart2077@gmail.com>
2024-04-09 23:51:09 +0900

    [ADD] Jump

--- Assets/RetoroUnity/Script/Jumper.cs
+++ Assets/RetoroUnity/Script/Jumper.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Jumper : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody myRigidbody;
     void Start()
     {
-        
+        myRigidbody.AddForce(0, 500, 0);
     }
 
     // Update is called once per frame
